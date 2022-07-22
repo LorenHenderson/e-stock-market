40590381546b4848de0837501238b22226885b18
unknown <cogjava772@iiht.tech>
2022-07-21 21:21:03 -0400

    initial commit

---
+++ stocks-domain/src/main/java/com/market/stock/stocksdomain/constants/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package com.market.stock.stocksdomain.constants;
+
+public class ApplicationConstants {
+    public static final String SAVE_STOCKS_COMMAND = "SaveStocks";
+    public static final String SAVE_STOCKS_TOPIC = "saveStocksCommand";
+}
